e6f55f5dc54198f638cae532c2962baa36cf920f
anusankanal <anusankanal@gmail.com>
2026-01-17 11:35:01 +0530

    Update test_swap.py

--- test_swap.py
+++ test_swap.py
@@ -2,7 +2,7 @@ from swap import swap
 
 def test_case1():
     result = swap(10, 12)
-    print("Result of test_case1:", result)
+    print("Result of test_case1:", result)   
     assert result == (12, 10)
 
 def test_case2():
@@ -13,4 +13,9 @@ def test_case2():
 def test_case3():
     result = swap(4, 5)
     print("Result of test_case3:", result)
-    assert result == (5, 4)  
+    assert result == (5, 4)
+
+if __name__ == "__main__":
+    test_case1()
+    test_case2()
+    test_case3()  
