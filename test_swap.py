56f1a2a7a3c8150e2253037683ed69edf123e473
Anusha S <anusankanal@gmail.com>
2026-01-17 11:01:36 +0530

    save

---
+++ test_swap.py
@@ -0,0 +1,10 @@
+from swap import swap
+
+def test_case1():
+    assert swap(10,12)==(12,10)
+  
+def test_case1():
+    assert swap(2,3)==(3,2)
+    
+def test_case1():
+    assert swap(4,5)==(5,14)  
